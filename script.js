9007d42cc425f7ce3d2d73b53c6e2a1b735d14d0
Sukanta Das <111263085+Sukantadas1@users.noreply.github.com>
2023-12-02 08:33:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+function updateSizeInfo() {
+  // Get the width and height of the window
+  const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  const height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+  // Update the content of the <h1> tag
+  const sizeDisplay = document.getElementById('sizeDisplay');
+  sizeDisplay.textContent = `Width: ${width} Height: ${height}`;
+}
+updateSizeInfo();
+
+// Add event listener for window resize
+window.addEventListener('resize', updateSizeInfo);
